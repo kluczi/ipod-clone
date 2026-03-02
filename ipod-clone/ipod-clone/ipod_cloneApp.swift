bcdf564d79a2a0a1d8d0eaf02e0ed334e543dcab
kluczi <bartekcsgo04@gmail.com>
2026-03-02 20:02:58 +0100

    Initial commit

---
+++ ipod-clone/ipod-clone/ipod_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ipod_cloneApp.swift
+//  ipod-clone
+//
+//  Created by Bartek on 02/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct ipod_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ipod-clone/ipod-cloneTests/ipod_cloneTests.swift
@@ -0,0 +1,17 @@
+//
+//  ipod_cloneTests.swift
+//  ipod-cloneTests
+//
+//  Created by Bartek on 02/03/2026.
+//
+
+import Testing
+@testable import ipod_clone
+
+struct ipod_cloneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
