b84cab2458be0de874aed7fb116e8ab91a63a4a7
kluczi <bartekcsgo04@gmail.com>
2026-03-03 09:11:57 +0100

    Add MenuScreenView
    Stub main menu screen for iPod UI.

---
+++ ipod-clone/ipod-clone/Screens/MenuScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  MenuScreenView.swift
+//  ipod-clone
+//
+//  Created by Bartek on 02/03/2026.
+//
+
+import SwiftUI
+
+struct MenuScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MenuScreenView()
+}
