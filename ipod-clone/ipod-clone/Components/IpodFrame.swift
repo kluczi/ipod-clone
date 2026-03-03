8d675b904689d82602c5dcac8745569f79ea8e08
kluczi <bartekcsgo04@gmail.com>
2026-03-03 09:11:43 +0100

    Implement IpodFrame component
    Render base ipod body background view.

---
+++ ipod-clone/ipod-clone/Components/IpodFrame.swift
@@ -0,0 +1,20 @@
+//
+//  IpodFrame.swift
+//  ipod-clone
+//
+//  Created by Bartek on 02/03/2026.
+//
+
+import SwiftUI
+
+struct IpodFrame: View {
+    var body: some View {
+        Color.clear
+            .background(Color(.ipodBody))
+            .ignoresSafeArea()
+    }
+}
+
+#Preview {
+    IpodFrame()
+}
