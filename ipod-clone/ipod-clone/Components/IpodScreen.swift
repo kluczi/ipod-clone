55212d1e0ae422f986e351a1f693ff2ee266ba09
kluczi <bartekcsgo04@gmail.com>
2026-03-03 09:11:57 +0100

    Implement IpodScreen component
    Add rounded screen glass view and IpodScreen type.

---
+++ ipod-clone/ipod-clone/Components/IpodScreen.swift
@@ -0,0 +1,22 @@
+//
+//  IpodScreen.swift
+//  ipod-clone
+//
+//  Created by Bartek on 02/03/2026.
+//
+
+import SwiftUI
+
+struct IpodScreen: View {
+    var body: some View {
+        RoundedRectangle(cornerRadius: 12)
+            .foregroundStyle(Color(.ipodScreenGlass))
+            .padding(.horizontal,16)
+            .padding(.vertical, 32)
+            .cornerRadius(12)
+    }
+}
+
+#Preview {
+    IpodScreen()
+}
