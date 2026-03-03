19323ebd3ddfb07d4a518fd3fdab303af9c4fde9
kluczi <bartekcsgo04@gmail.com>
2026-03-03 09:11:43 +0100

    Add ListRow component
    Introduce reusable list row view for menus.

---
+++ ipod-clone/ipod-clone/Components/ListRow.swift
@@ -0,0 +1,18 @@
+//
+//  ListRow.swift
+//  ipod-clone
+//
+//  Created by Bartek on 02/03/2026.
+//
+
+import SwiftUI
+
+struct ListRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListRow()
+}
