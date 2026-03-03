45c5477c1e08d93a1bcbdb458c2639124c76284e
kluczi <bartekcsgo04@gmail.com>
2026-03-03 09:11:58 +0100

    Add NowPlayingScreenView
    Stub now playing screen for iPod UI.

---
+++ ipod-clone/ipod-clone/Screens/NowPlayingScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  NowPlayingScreenView.swift
+//  ipod-clone
+//
+//  Created by Bartek on 02/03/2026.
+//
+
+import SwiftUI
+
+struct NowPlayingScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NowPlayingScreenView()
+}
