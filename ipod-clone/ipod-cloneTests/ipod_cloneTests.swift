434054798118f5a60bbbd9b379fd483309489661
kluczi <bartekcsgo04@gmail.com>
2026-03-05 20:37:14 +0100

    feat(assets-list-row): add list row asset catalog

--- ipod-clone/ipod-cloneTests/ipod_cloneTests.swift
+++ ipod-clone/ipod-cloneTests/ipod_cloneTests.swift
@@ -5,13 +5,11 @@
 //  Created by Bartek on 02/03/2026.
 //
 
-import Testing
 @testable import ipod_clone
+import Testing
 
 struct ipod_cloneTests {
-
-    @Test func example() async throws {
+    @Test func example() {
         // Write your test here and use APIs like `#expect(...)` to check expected conditions.
     }
-
 }
---
+++ somos/lab1/somos-lab-1/somos-lab-1/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  somos-lab-1
+//
+//  Created by Bartek on 05/03/2026.
+//
+
+import Foundation
+
+print("Hello, World!")
+
