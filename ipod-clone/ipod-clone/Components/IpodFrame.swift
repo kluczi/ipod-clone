e5cc26f8562a9a6ae344fdd442e5cc813f0173f5
kluczi <bartekcsgo04@gmail.com>
2026-03-05 20:37:15 +0100

    refactor(shell-frame): update ipod frame gradients

--- ipod-clone/ipod-clone/Components/IpodFrame.swift
+++ ipod-clone/ipod-clone/Components/IpodFrame.swift
@@ -9,9 +9,28 @@ import SwiftUI
 
 struct IpodFrame: View {
     var body: some View {
-        Color.clear
-            .background(Color(.ipodBody))
-            .ignoresSafeArea()
+        RadialGradient(
+            colors: [
+                Color("ipodBodyUpperMid"),
+                Color("ipodBodyBase"),
+                Color("ipodBodyBottom"),
+            ],
+            center: .init(x: 0.5, y: 0.35),
+            startRadius: 30,
+            endRadius: 450
+        )
+        .overlay(
+            RadialGradient(
+                colors: [
+                    Color.white.opacity(0.1),
+                    Color.clear,
+                ],
+                center: .top,
+                startRadius: 20,
+                endRadius: 350
+            )
+        )
+        .ignoresSafeArea()
     }
 }
 
