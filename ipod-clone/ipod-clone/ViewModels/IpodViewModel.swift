c9d1fc8f019f836d8c2b32d245bc685d668493ae
kluczi <bartekcsgo04@gmail.com>
2026-03-05 20:37:15 +0100

    refactor(view-model): adjust navigation and player state

--- ipod-clone/ipod-clone/ViewModels/IpodViewModel.swift
+++ ipod-clone/ipod-clone/ViewModels/IpodViewModel.swift
@@ -8,7 +8,7 @@
 import Combine
 import Foundation
 
-//routing+vm
+// routing+vm
 
 class IpodViewModel: ObservableObject {
     @Published var navigationStack: [Screen] = [.menu(selectedIndex: 0)]
@@ -25,19 +25,19 @@ class IpodViewModel: ObservableObject {
     var currentScreen: Screen {
         navigationStack.last ?? .menu(selectedIndex: 0)
     }
-    
-    //func to handle routing
+
+    /// func to handle routing
     func handleRotate(delta: Int) {
         guard !navigationStack.isEmpty else { return }
 
         switch currentScreen {
-        case .library(let selectedIndex):
+        case let .library(selectedIndex):
             guard !tracks.isEmpty else { return }
             let newIndex = selectedIndex + delta
             let clampedIndex = min(max(0, newIndex), tracks.count - 1)
             navigationStack[navigationStack.count - 1] = .library(selectedIndex: clampedIndex)
 
-        case .menu(let selectedIndex):
+        case let .menu(selectedIndex):
             guard !menuItems.isEmpty else { return }
             let newIndex = selectedIndex + delta
             let clampedIndex = min(max(0, newIndex), menuItems.count - 1)
@@ -50,12 +50,12 @@ class IpodViewModel: ObservableObject {
 
     func handleSelect() {
         switch currentScreen {
-        case .menu(selectedIndex: let index):
+        case let .menu(selectedIndex: index):
             guard menuItems.indices.contains(index) else { return }
             let destination = menuItems[index].destination
             navigationStack.append(destination)
 
-        case .library(selectedIndex: let index):
+        case let .library(selectedIndex: index):
             guard tracks.indices.contains(index) else { return }
             let trackId = tracks[index].id
             playerState.currentTrackId = trackId
