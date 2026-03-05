b4db4591d9d9ed2241b9a789cf93cd00b644bc16
kluczi <bartekcsgo04@gmail.com>
2026-03-05 20:37:15 +0100

    refactor(components): enhance list row appearance

--- ipod-clone/ipod-clone/Components/ListRow.swift
+++ ipod-clone/ipod-clone/Components/ListRow.swift
@@ -7,12 +7,51 @@
 
 import SwiftUI
 
-struct ListRow: View {
+struct ListRow<Trailing:View>: View {
+    let title: String
+    let subtitle: String?
+    let leadingImage: String?
+    let isSelected: Bool
+    @ViewBuilder let trailing: () -> Trailing
+    
+    var titleColor: Color {
+        isSelected ? .titleSelected:.titlePrimary
+    }
+    var subtitleColor: Color {
+        isSelected ? .subTitleSelected : .subTitlePrimary
+    }
+    var backgroundColor: Color {
+        isSelected ? .rowBackgroundSelected:.rowBackground
+    }
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            if let leadingImage {
+                Image(leadingImage)
+                    .resizable()
+                    .frame(width: 64, height: 64)
+                    .clipShape(RoundedRectangle(cornerRadius: 6))
+            }
+            
+            VStack(alignment: .leading) {
+                Text(title)
+                    .foregroundStyle(titleColor)
+                    .fontWeight(.bold)
+                if let subtitle {
+                    Text(subtitle)
+                        .foregroundStyle(subtitleColor)
+                }
+            }
+            Spacer()
+            trailing()
+                .foregroundStyle(titleColor)
+
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .padding(12)
+        .background(backgroundColor)
     }
 }
 
 #Preview {
-    ListRow()
+    ListRow(title: "fml .", subtitle: "fakemink", leadingImage: "placeholder", isSelected: false){}
 }
