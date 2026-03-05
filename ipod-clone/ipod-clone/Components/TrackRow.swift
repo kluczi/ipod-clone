41927839ccd5a71aff7435413346e18f91553c2c
kluczi <bartekcsgo04@gmail.com>
2026-03-05 20:37:15 +0100

    feat(components): add track row view

---
+++ ipod-clone/ipod-clone/Components/TrackRow.swift
@@ -0,0 +1,30 @@
+//
+//  TrackRow.swift
+//  ipod-clone
+//
+//  Created by Bartek on 05/03/2026.
+//
+
+import SwiftUI
+
+struct TrackRow: View {
+    let track: Track
+    let isSelected: Bool
+    var body: some View {
+        ListRow(title: track.title, subtitle: track.artist, leadingImage: track.image ,isSelected: isSelected) {
+            Text(track.formattedDuration)
+        }
+    }
+}
+
+#Preview {
+    let example = Track(
+            id: UUID(),
+            title: "fml .",
+            artist: "fakemink",
+            albumTitle: "The Boy who cried Terrified.",
+            duration: 163,
+            image: "placeholder"
+        )
+    TrackRow(track: example, isSelected: true)
+}
