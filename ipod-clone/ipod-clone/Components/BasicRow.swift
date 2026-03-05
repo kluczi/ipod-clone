12bb06019bc800aae3b37b74cfbd3aac440b2d74
kluczi <bartekcsgo04@gmail.com>
2026-03-05 20:37:15 +0100

    feat(components): add basic row view

---
+++ ipod-clone/ipod-clone/Components/BasicRow.swift
@@ -0,0 +1,31 @@
+//
+//  BasicRow.swift
+//  ipod-clone
+//
+//  Created by Bartek on 05/03/2026.
+//
+
+import SwiftUI
+
+struct BasicRow: View {
+    let item: MenuItem
+    var isSelected: Bool
+    var body: some View {
+        ListRow(title: item.title, subtitle: nil, leadingImage: nil, isSelected: isSelected) {
+            Button {
+                
+            } label: {
+                Image(systemName: "chevron.right")
+            }
+        }
+    }
+}
+
+#Preview {
+    let example = MenuItem (
+        id: UUID(),
+        title: "Library",
+        destination: .library(selectedIndex: 1)
+    )
+    BasicRow(item: example, isSelected: true)
+}
