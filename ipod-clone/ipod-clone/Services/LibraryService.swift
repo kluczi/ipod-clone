45596eb1acdf40a9ab7d41e069be71b1a5b90ae3
kluczi <bartekcsgo04@gmail.com>
2026-03-06 23:44:17 +0100

    feat(services): add library and playback services

---
+++ ipod-clone/ipod-clone/Services/LibraryService.swift
@@ -0,0 +1,10 @@
+//
+//  LibraryService.swift
+//  ipod-clone
+//
+//  Created by Bartek on 06/03/2026.
+//
+
+final class LibraryService {
+    
+}
---
+++ ipod-clone/ipod-clone/Services/PlaybackService.swift
@@ -0,0 +1,44 @@
+//
+//  PlaybackService.swift
+//  ipod-clone
+//
+//  Created by Bartek on 06/03/2026.
+//
+
+import AVFoundation
+import Combine
+
+final class PlaybackService {
+    private var player: AVPlayer
+
+    init() {
+        self.player=AVPlayer()
+        configureAudioSession()
+    }
+    
+    private func configureAudioSession() {
+            let session=AVAudioSession.sharedInstance()
+            try? session.setCategory(.playback, mode: .default)
+            try? session.setActive(true)
+    }
+    
+    func load(track: Track) {
+        guard let url = Bundle.main.url(forResource: track.fileName, withExtension: "mp3") else {return}
+        let item = AVPlayerItem(url: url)
+        player.replaceCurrentItem(with: item)
+    }
+    
+    func play() {
+        player.play()
+    }
+    
+    func pause() {
+        player.pause()
+    }
+    
+    func currentTime() -> TimeInterval {
+        player.currentTime().seconds
+    }
+    
+    
+}
